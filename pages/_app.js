509d968e1dd54a35c7fa1b915656473160d1e7b8
prachi <darshana@kubric.io>
2021-07-09 15:19:45 +0530

    next.js pwa app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <> 
+  <Head>
+      <meta charSet='utf-8' />
+      <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+      <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+      <meta name='description' content='Description' />
+      <title>Next.js PWA</title>
+      <link rel='manifest' href='/manifest.json' />
+      <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+      <link href='/favicon-32x32.png' rel='icon' type='image/png'    sizes='32x32' />
+      <link rel='apple-touch-icon' href='/logo1.png'></link>
+      <meta name='theme-color' content='#21125e' />
+ </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
